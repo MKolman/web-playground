b4fccc8f96dc98a9c860b894de0590193cf1f9dd
Maks Kolman <kolman.maks@gmail.com>
2017-04-15 21:53:01 +0200

    Moved to amazon

--- main.py
+++ main.py
@@ -3,29 +3,29 @@ from flask import Flask, render_template, request, make_response, session, redir
 import devel
 import broken
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 
-@app.route("/")
+@application.route("/")
 def root():
     return render_template("index.html")
 
 
-@app.route("/logout")
+@application.route("/logout")
 def logout():
     if "user" in session:
         del session["user"]
     return redirect("/login")
 
 
-app.add_url_rule("/reset_db", 'reset_db', devel.reset_db)
-app.add_url_rule("/login", 'login', broken.login)
-app.add_url_rule("/bad_search", 'bad_search', broken.bad_search)
-app.add_url_rule("/evil", 'evil', broken.evil)
-app.add_url_rule("/forum", 'forum', broken.forum)
-app.add_url_rule("/db_viewer_XDSie983BQbnxc_asjdh", 'db_view', broken.db_view)
-app.secret_key = 'password'
-app.config.update(SESSION_COOKIE_HTTPONLY=False)
+application.add_url_rule("/reset_db", 'reset_db', devel.reset_db)
+application.add_url_rule("/login", 'login', broken.login)
+application.add_url_rule("/bad_search", 'bad_search', broken.bad_search)
+application.add_url_rule("/evil", 'evil', broken.evil)
+application.add_url_rule("/forum", 'forum', broken.forum)
+application.add_url_rule("/db_viewer_XDSie983BQbnxc_asjdh", 'db_view', broken.db_view)
+application.secret_key = 'password'
+application.config.update(SESSION_COOKIE_HTTPONLY=False, DEBUG=True)
 
 if __name__ == "__main__":
-    app.run()
+    application.run()
